710fd2d72186704715086f848188ca8635ff2e83
Saulė Abramovičiūtė <abramoviciutesaule@gmail.com>
2022-10-08 19:14:24 +0300

    Add files via upload

---
+++ Controllers/PossibleConversionsController.cs
@@ -0,0 +1,17 @@
+﻿using CurrConverter.Models.PossibleConversions;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CurrConverter.Controllers
+{
+    public class PossibleConversions : ControllerBase
+    {
+        // [ProducesResponseType(typeof(CurrencyConvertor), StatusCodes.Status200OK)]
+        [HttpGet("PossibleConversions")]
+        public IActionResult Get([Required] PossibleConversionsRequest currencyConversionRequest)
+        {
+            PossibleConversionsProvider outputCurrencyAndAmountProvider = new PossibleConversionsProvider();
+            return Ok(outputCurrencyAndAmountProvider.PrintAllPossibleConversions(currencyConversionRequest));
+        }
+    }
+}
---
+++ Controllers/TradeController.cs
@@ -0,0 +1,29 @@
+﻿using CurrConverter.Models;
+using CurrConverter.Models.Trades;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CurrConverter.Controllers
+{
+    public class TradeController : Controller
+    {
+        [HttpPost("NewTrade")]
+        public IActionResult Post([Required][FromBody] AmountConversionRequest amountConversionRequest)
+        {
+            TradesRepository tradesRepository = new TradesRepository("Server=localhost;Database=TestCurrencyConverter;Trusted_Connection=True;TrustServerCertificate=true;");
+
+            AmountConversion amountConversion = new AmountConversion();
+            Trade trade = new Trade
+            {
+                FromAmount = amountConversionRequest.FromAmount,
+                ToCurrency = amountConversionRequest.ToCurrency,
+                FromCurrency = amountConversionRequest.FromCurrency,
+                Client = amountConversionRequest.Client,
+                Rate = amountConversion.GetCurrencyCombinationRate(amountConversionRequest),
+                ConvertedAmount = amountConversion.AmountConvertor(amountConversionRequest)
+            };
+            tradesRepository.Insert(trade);
+            return Ok();
+        }
+    }
+}
